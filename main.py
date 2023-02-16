d5cd18919afa9a43fa4e659dc4d8d1a1f32e5cd2
sccthomas <95318179+sccthomas@users.noreply.github.com>
2023-02-16 19:20:50 +0000

    almost done

--- main.py
+++ main.py
@@ -1,16 +1,86 @@
-# This is a sample Python script.
+def mazeToArray(file: str) -> [[str]]:
+    maze = []
+    maze_file = open("mazes/" + file, "r")
+    rows = maze_file.readlines()
+    for row in rows:
+        if len(row) > 1:
+            row_final = []
+            for item in list(row):
+                if item != ' ':
+                    row_final.append(item)
+            row_final.remove('\n')
+            maze.append(row_final)
+    return maze
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def findStartEnd(maze: [[str]]) -> tuple[[int], [int]]:
+    start = maze[0]
+    end = maze[len(maze) - 1]
+    start_coords = [0, start.index('-')]
+    end_coords = [len(maze) - 1, end.index('-')]
+    return start_coords, end_coords
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def findPath(maze: [[str]], start: [int], end: [int]) -> [[int]]:
+    stack = []
+    visited = []
+    path = []
+    stack.append(start)
+    # if there are nodes around that are not visited then we push to path
+    while stack:
+        current_node = stack.pop()
+        x = current_node[0]
+        y = current_node[1]
+
+        # we try to find all directions that we can go
+        try:
+            if maze[x+1][y] == '-':  # down
+                if [x+1, y] not in visited:
+                    stack.append([x+1, y])
+        except:
+            pass
+        try:
+            if maze[x][y-1] == '-':  # left
+                if [x, y-1] not in visited:
+                    stack.append([x, y-1])
+        except:
+            pass
+        try:
+            if maze[x-1][y] == '-':  # up
+                if [x-1, y] not in visited:
+                    stack.append([x-1, y])
+        except:
+            pass
+        try:
+            if maze[x][y+1] == '-':  # right
+                if [x, y+1] not in visited:
+                    stack.append([x, y+1])
+        except:
+            pass
+
+        visited.append(current_node)
+        if [x, y] == end:
+            print("found!")
+            return visited
+
+def printMaze(path:[[str]], maze:[[str]]):
+    for i in range(0, len(maze)):
+        for j in range(0, len(maze[i])):
+            if maze[i][j] != '#':
+                maze[i][j] = ' '
+    for coord in path:
+        x = coord[0]
+        y = coord[1]
+        maze[x][y] = 'X'
+    for row in maze:
+        for i in range(0, len(row)*2, 2):
+            row.insert(i, ' ')
+        print((', '.join(map(str, row))).replace(',',""))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # make a user input bit where they input the mazes that they want to traverse
+    maze = mazeToArray("maze-Easy.txt")
+    start, end = findStartEnd(maze)
+    path = findPath(maze, start, end)
+    printMaze(path, maze)
